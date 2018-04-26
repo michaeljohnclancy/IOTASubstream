005ed26c7ad9e0d2a62c1dadb6121338a044936e
Michael Clancy <mclancy@pc-139-250.customer.elliott.crm.lan>
2018-04-26 22:01:22 +0100

    New Changes

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from iota import *
 from time import *
 import thread
 import time
-import flask_login as fl
+from flask_login import LoginManager
+from flask_security import login_required
+
 
 
 from modules import User, sql
 
 app = Flask(__name__)
-LoginManager = fl.LoginManager(app)
+LoginManager = LoginManager(app)
 LoginManager.login_view = "login"
 
 @LoginManager.user_loader
 def load_user(userID):
-	return User(userID)
+	new_user = User(userID)
+	if  new_user.exists():
+		return new_user
+	else:
+		return None
 
 @app.route("/")
 def main():
@@ -50,16 +56,32 @@ def userSignup():
 		_userID = str(request.form['userID'])
 		newUser = User(_userID)
 		newUser.setNode("http://node02.iotatoken.nl:14265")
+		password = str(request.form['password'])
 
-		if str(request.form['password']) == str(request.form['confirm']):
-			_password_hash = newUser.password_encrypt(str(request.form['password']))
+		passwords_match = (password == str(request.form['confirm']))
+
+		if ((not newUser.exists()) and (passwords_match)):
+			_password_hash = newUser.password_encrypt(password)
 		
 			_email = str(request.form['email'])
 			newUser.commitUserToDB(_email, _password_hash)
 			newUser.set_is_active(True) #Should wait until email is confirmed in the future, set to 1 in database
 			newUser.set_is_anonymous(False)
 
-			print("Done")
+			next = flask.request.args.get('next')
+
+			if not is_safe_url(next):
+				return flask.abort(400)
+
+			return flask.redirect(next or flask.url_for('/signup_success'))
+
+		elif not passwords_match:
+			flash("Passwords dont match!")
+			return flask.redirect(flask.url_for('/signup'))
+		elif newUser(exists) or newUser.db_connection.emailTaken(_email):
+			flash("User or Email taken")
+			return flask.redirect(flask.url_for('/signup'))
+
 	return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -68,7 +90,7 @@ def login():
 		_userID = str(request.form['userID'])
 		_password = str(request.form['password'])
 		user = User(_userID)
-		if (user.password_verify(_password, user.get_password_hash())):
+		if (user.exists and user.password_verify(_password, user.get_password_hash())):
 			user.setNode("http://node02.iotatoken.nl:14265")
 			user.set_is_authenticated(True)
 			
@@ -80,17 +102,22 @@ def login():
 			if not is_safe_url(next):
 				return flask.abort(400)
 
-			return flask.redirect(next or flask.url_for('/'))
+			return flask.redirect(next or flask.url_for('/yourstats'))
+
 	return render_template('login.html')
 
+
 @app.route('/yourstats', methods=['GET', 'POST'])
+@login_required
 def viewStats():
 	return render_template('index.html')
 
+@app.route('/signup_success', methods=['GET', 'POST'])
+def success():
+	return render_template('signup_success.html')
 
 
-
-
+app.secret_key = ""
 
 
 if __name__ == "__main__":
