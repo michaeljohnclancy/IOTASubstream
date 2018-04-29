e3b1eacd698bb206a337287e491823d8a51ab99a
Michael Clancy <mclancy@pc-139-250.customer.elliott.crm.lan>
2018-04-29 15:43:00 +0100

    rm instance

--- instance/config.py
+++
@@ -1,4 +0,0 @@
-# instance/config.py
-
-SECRET_KEY = 'UD\x01\xa2B\xe1G\xf6\t\xfaeA\x18<o\xe0>8S\x08\x84\x14tX'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://iota_admin:iota_admin@localhost/transactions_db'
